18433cad8d74f693fb7e9cab8ec3f008d54bcca6
SASANO Takayoshi <uaa@uaa.org.uk>
2021-08-12 17:55:35 +0900

    add MSX2 support

--- mkdata.c
+++ mkdata.c
@@ -14,16 +14,26 @@ static uint8_t buf[X_WIDTH * Y_WIDTH * 3];
 // size of display and position of original picture
 #define X_POS	1
 #define Y_POS	1
-#define	X_SIZE	32
+#define X_SIZE	32
 #define Y_SIZE	24
 
 // reference: https://paulwratt.github.io/programmers-palettes/HW-MSX/HW-MSX-palettes.html
-static uint32_t msx_palette[] = {
+#define PALETTE_MAX	16
+static uint32_t msx_palette[PALETTE_MAX] = {
 	0x000000, 0x010101, 0x3eb849, 0x74d07d,
 	0x5955e0, 0x8076f1, 0xb95e51, 0x65dbef,
 	0xdb6559, 0xff897d, 0xccc35e, 0xded087,
 	0x3aa241, 0xb766b5, 0xcccccc, 0xffffff,
 };
+
+static uint32_t palette_data[PALETTE_MAX];
+static uint32_t palette_dist[PALETTE_MAX];
+
+static void init_palette(void)
+{
+	memset(palette_data, 0, sizeof(palette_data));
+	memset(palette_dist, ~0, sizeof(palette_dist));
+}
 
 static int find_near_color(uint32_t col)
 {
@@ -38,7 +48,7 @@ static int find_near_color(uint32_t col)
 
 	dx = ~0;
 	ix = -1;
-	for (i = 0; i < sizeof(msx_palette) / sizeof(uint32_t); i++) {
+	for (i = 0; i < PALETTE_MAX; i++) {
 		r1 = (msx_palette[i] >> 16) & 0xff;
 		g1 = (msx_palette[i] >> 8) & 0xff;
 		b1 = msx_palette[i] & 0xff;
@@ -54,7 +64,26 @@ static int find_near_color(uint32_t col)
 		}
 	}
 
+	if (d < palette_dist[ix])
+		palette_data[ix] = col;
+
 	return ix;
+}
+
+static void dump_palette(void)
+{
+	int i;
+	uint8_t r, g, b;
+
+	for (i = 0; i < PALETTE_MAX; i++) {
+		r = (palette_data[i] >> 21) & 7;
+		g = (palette_data[i] >> 13) & 7;
+		b = (palette_data[i] >> 5) & 7;
+
+		printf("%d,%d, ", (r << 4) | b, g);
+	}
+
+	printf("\n");
 }
 
 static void dump(char *filename)
@@ -62,8 +91,6 @@ static void dump(char *filename)
 	FILE *fp;
 	int32_t x, y, c;
 	uint8_t *p;
-
-	printf("	/* %s */\n", filename);
 
 	memset(buf, 0, sizeof(buf));
 	fp = fopen(filename, "r");
@@ -102,11 +129,23 @@ int main(int argc, char *argv[])
 
 	printf("// this is auto-generated, do not edit!\n");
 	printf("\n");
-	printf("const unsigned char parrot_data[] = {\n");
 
-	for (i = 1; i < argc; i++)
+	init_palette();
+
+	for (i = 1; i < argc; i++) {
+		printf("/* %s */\n", argv[i]);
+		printf("const unsigned char parrot_data%02d[] = {\n", i - 1);
 		dump(argv[i]);
+		printf("};\n\n");
+	}
 
+	printf("const unsigned char *parrot_data[] = {\n");
+	for (i = 1; i < argc; i++)
+		printf("\tparrot_data%02d,\n", i - 1);
+	printf("};\n\n");
+
+	printf("const unsigned char parrot_palette[] = {\n");
+	dump_palette();
 	printf("};\n");
 
 	return 0;
--- parrot_code.c
+++ parrot_code.c
@@ -3,16 +3,24 @@
 
 #include <msx/gfx.h>
 
-extern const unsigned char parrot_data[];
+extern const unsigned char *parrot_data[];
+extern const unsigned char parrot_palette[];
 
 #define DATA_SIZE	768
 #define PLANE		10
 
-#define	DELAY		500
+#define DELAY		500
 
-#define	PATTERN_GEN	0x0000	// pattern generator table
-#define	PATTERN_NAME	0x1800	// pattern name table
+#define PATTERN_GEN	0x0000	// pattern generator table
+#define PATTERN_NAME	0x1800	// pattern name table
 #define COLOR_TABLE	0x2000	// color table
+
+#define PALETTE_MAX	16
+
+#define VDP_PORT_R	0x0006	// VDP I/O port (read) stored address
+#define VDP_PORT_W	0x0007	// VDP I/O port (write) stored address
+#define MSX_VERSION	0x002d	// MSX-BASIC version stored address
+#define NEWKEY		0xfbe5	// keyboard matrix status
 
 static void init_screen(void)
 {
@@ -38,13 +46,35 @@ static void dummy_loop(int ticks)
 	}
 }
 
+static void set_palette(const unsigned char *palette)
+{
+	int i;
+	unsigned int port = *(unsigned int *)VDP_PORT_W;
+
+#asm
+	di
+#endasm
+
+	/* set palette index to 0 */
+	outp(port + 1, 0x00);
+	outp(port + 1, 0x90);
+
+	/* send palette data */
+	for (i = 0; i < PALETTE_MAX; i++) {
+		outp(port + 2, *palette++);
+		outp(port + 2, *palette++);
+	}
+#asm
+	ei
+#endasm
+}
 
 static void do_disp(void)
 {
 	int i;
 
 	for (i = 0; i < PLANE; i++) {
-		vwrite(parrot_data + DATA_SIZE * i, PATTERN_NAME, DATA_SIZE);
+		vwrite(parrot_data[i], PATTERN_NAME, DATA_SIZE);
 		dummy_loop(DELAY);
 	}
 }
@@ -52,6 +82,11 @@ static void do_disp(void)
 int main(int argc, char *argv[])
 {
 	init_screen();
+
+	/* MSX1 or press "1"; no palette setting */
+	if ((*((unsigned char *)NEWKEY) & 2) && *((unsigned char *)MSX_VERSION))
+		set_palette(parrot_palette);
+
 	while (1)
 		do_disp();
 
